7b97cbeb3e73426f832c156327fa6227167488d1
Shubham Kumar <shubhamkr75@gmail.com>
2021-05-01 08:25:30 +0530

    AutoLoading response if the test is restarted

--- exambaba/src/Component/Questions.js
+++ exambaba/src/Component/Questions.js
@@ -38,21 +38,21 @@ class Questions extends Component {
                 <div class="questionList">
                 {this.state.questionList.map((qlist) => {
                     return(
-                <table class="questionSection" cellspacing="0" cellpadding="0" border="0" width="100%">
-                <tbody><tr>
-                    <td class="bix-td-qno" rowspan="2" valign="top" align="left"><p>{qlist.id}.&nbsp;</p></td>
-                    <td class="bix-td-qtxt" valign="top"><p>{qlist.QuestionDesc}</p></td>
-                </tr>
-                <tr>
-                    <td  valign="top"><table border="0" cellpadding="0" cellspacing="0" width="100%"><tbody><tr><td width="1%" id="tdOptionNo_A_274"><input type="radio" Name={qlist.id} onClick={(e)=>this.saveResponse(e,qlist.id,1)}/></td><td class="bix-td-option" width="1%" id="tdOptionNo_A_434"><a id="lnkOptionLink_A_434" href="javascript: void 0;">A.</a></td>
-                    <td  width="99%" >{qlist.Option1}</td></tr><tr><td width="1%" id="tdOptionNo_A_274"><input type="radio" Name={qlist.id} onClick={(e)=>this.saveResponse(e,qlist.id,2)}/></td><td width="1%" ><a id="lnkOptionLink_B_434" href="javascript: void 0;">B.</a></td>
-                    <td width="99%" >{qlist.Option2}</td></tr><tr><td width="1%" id="tdOptionNo_A_274"><input type="radio" Name={qlist.id} onClick={(e)=>this.saveResponse(e,qlist.id,3)}/></td><td  width="1%" ><a id="lnkOptionLink_C_434" href="javascript: void 0;">C.</a></td>
-                    <td width="99%" >{qlist.Option3}</td></tr><tr><td width="1%" id="tdOptionNo_A_274"><input type="radio" Name={qlist.id} onClick={(e)=>this.saveResponse(e,qlist.id,4)} /></td><td  width="1%"><a id="lnkOptionLink_D_434" href="javascript: void 0;">D.</a></td>
-                    <td width="99%" >{qlist.Option4}</td></tr></tbody></table>                    
-                    </td>
-                </tr>
-                </tbody></table> 
-                );
+                        <table class="questionSection" cellspacing="0" cellpadding="0" border="0" width="100%">
+                        <tbody><tr>
+                            <td class="bix-td-qno" rowspan="2" valign="top" align="left"><p>{qlist.id}.&nbsp;</p></td>
+                            <td class="bix-td-qtxt" valign="top"><p>{qlist.QuestionDesc}</p></td>
+                        </tr>
+                        <tr>
+                            <td  valign="top"><table border="0" cellpadding="0" cellspacing="0" width="100%"><tbody><tr><td width="1%" id="tdOptionNo_A_274"><input type="radio" checked={this.state.answerList[qlist.id]===1} Name={qlist.id} onClick={(e)=>this.saveResponse(e,qlist.id,1)}/></td><td class="bix-td-option" width="1%" id="tdOptionNo_A_434"><a id="lnkOptionLink_A_434" href="javascript: void 0;">A.</a></td>
+                            <td  width="99%" >{qlist.Option1}</td></tr><tr><td width="1%" id="tdOptionNo_A_274"><input type="radio" checked={this.state.answerList[qlist.id]==2} Name={qlist.id} onClick={(e)=>this.saveResponse(e,qlist.id,2)}/></td><td width="1%" ><a id="lnkOptionLink_B_434" href="javascript: void 0;">B.</a></td>
+                            <td width="99%" >{qlist.Option2}</td></tr><tr><td width="1%" id="tdOptionNo_A_274"><input type="radio" checked={this.state.answerList[qlist.id]===3} Name={qlist.id} onClick={(e)=>this.saveResponse(e,qlist.id,3)}/></td><td  width="1%" ><a id="lnkOptionLink_C_434" href="javascript: void 0;">C.</a></td>
+                            <td width="99%" >{qlist.Option3}</td></tr><tr><td width="1%" id="tdOptionNo_A_274"><input type="radio" checked={this.state.answerList[qlist.id]===4} Name={qlist.id} onClick={(e)=>this.saveResponse(e,qlist.id,4)} /></td><td  width="1%"><a id="lnkOptionLink_D_434" href="javascript: void 0;">D.</a></td>
+                            <td width="99%" >{qlist.Option4}</td></tr></tbody></table>                    
+                            </td>
+                        </tr>
+                        </tbody></table> 
+                    );
             })} 
             </div>
         );        
@@ -129,7 +129,8 @@ class Questions extends Component {
         .then((res) => res.json())
         .then((data) => {           
            console.log(data); 
-           res=data.recordset.length;              
+           res=data.recordset.length; 
+           this.setState({answerList:JSON.parse(data.recordset[0].ResponseAnswer)});            
         });
         return res;
     }
