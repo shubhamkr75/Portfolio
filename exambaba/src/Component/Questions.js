2ca166d524ce2f3e8ee851f06ddb3857fd7e7bc3
Shubham Kumar <shubhamkr75@gmail.com>
2021-06-15 06:50:07 +0530

    submit test popup

--- exambaba/src/Component/Questions.js
+++ exambaba/src/Component/Questions.js
@@ -130,7 +130,7 @@ class Questions extends Component {
                     <div id="timeLeftDiv">
                         Time Left: <span id="timeLeft"></span>
                     </div>
-                    <input class="test-button" align="center" onClick={() => this.calculateMarks()} type="button" value="Submit Test" id="btnSubmitTest" />
+                    <input class="test-button" align="center" onClick={() => this.displayPopup()} type="button" value="Submit Test" id="btnSubmitTest" />
                 </div>
                 <div className="status-content">
                     <div className="status-available">
@@ -365,6 +365,13 @@ class Questions extends Component {
             }
         }, 1000);
     }
+    closePopup(){
+        document.getElementById("details-modal").style.display="none";
+    }
+    displayPopup(){
+        if(document.getElementById("details-modal"))
+        document.getElementById("details-modal").style.display="flex";
+    }
     // }
     render() {
         { (!this.state.examdata) && this.fetchExams() }
@@ -375,6 +382,24 @@ class Questions extends Component {
                 <div class="exam-page">
                     {this.displayQuestions()}
                     {this.questionStatus()}
+                    <div className="details-modal" id="details-modal">
+                        <div className="details-modal-close" onClick={this.closePopup}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M13.7071 1.70711C14.0976 1.31658 14.0976 0.683417 13.7071 0.292893C13.3166 -0.0976311 12.6834 -0.0976311 12.2929 0.292893L7 5.58579L1.70711 0.292893C1.31658 -0.0976311 0.683417 -0.0976311 0.292893 0.292893C-0.0976311 0.683417 -0.0976311 1.31658 0.292893 1.70711L5.58579 7L0.292893 12.2929C-0.0976311 12.6834 -0.0976311 13.3166 0.292893 13.7071C0.683417 14.0976 1.31658 14.0976 1.70711 13.7071L7 8.41421L12.2929 13.7071C12.6834 14.0976 13.3166 14.0976 13.7071 13.7071C14.0976 13.3166 14.0976 12.6834 13.7071 12.2929L8.41421 7L13.7071 1.70711Z" fill="black" />
+                            </svg>
+                        </div>
+                        <div className="details-modal-title">
+                            <h1>Submit</h1>
+                        </div>
+                        <div className="details-modal-content">
+                            Are you sure you want to submit the test?
+                        </div>
+                        <div className="action-button">
+                            <button className="submit-button error-background" onClick={()=>this.calculateMarks()}  value="Confirm" id="confirm">Confirm</button>
+                            <button className="submit-button"value="Cancel" onClick={this.closePopup} id="">Cancel</button>
+                            
+                        </div>               
+                    </div>
                 </div>
             );
 
